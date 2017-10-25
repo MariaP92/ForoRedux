115ee67ce0d32c580b075ce71fe4e336f841e24e
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-10-25 14:43:59 -0700

    foro2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from "redux-zero/react";
-import { addComment} from "./actions";
+import { addComment } from "./actions";
 import './App.css';
 
 const Comment = ({ image, name, comment }) => {
   return (
     <div>
-      <img src={image} className="sizeImg"/>
+      <img src={image} className="sizeImg" />
       <h3>{name}</h3>
+      <hr />
       <p>{comment}</p>
     </div>
   );
@@ -28,25 +29,22 @@ const App = ({ comments }) => {
   });
 
   const onSubmit = (e) => {
-		e.preventDefault();
-		addComment(this.nameInputRef.value,this.commentInputRef.value)
-	}
+    e.preventDefault();
+    addComment(this.nameInputRef.value, this.commentInputRef.value)
+  }
 
   return (
 
-    <div className="tab-pane col-md-6" id="add-comment">
-      <form onSubmit = {onSubmit}>
-        <div className="form-group">
-          <input className="col-sm-12 col-md-12" placeholder="Name" ref={(e) => this.nameInputRef = e} ></input>
-          <div className="col-sm-12 col-md-12">
+    <div className=" ContainerComments col-md-6" id="add-comment">
+      <form onSubmit={onSubmit}>
+        <div className="inputComments">
+          <div>
+            <input className="col-sm-12 col-md-12" placeholder="Name" ref={(e) => this.nameInputRef = e} ></input>
             <textarea className="form-control" name="addComment" id="addComment" rows="5" ref={(e) => this.commentInputRef = e}></textarea>
           </div>
-        </div>
-        <div className="form-group">
-          <div class="col-sm-offset-2 col-sm-10">
+          <div className="inputs">
             <button className="btn btn-success btn-circle text-uppercase" id="submitComment"><span class="glyphicon glyphicon-send"></span> Summit comment</button>
-          </div>
-        </div>
+          </div></div>
       </form>
       <div className="Comments">
         {UserComments}
@@ -55,5 +53,5 @@ const App = ({ comments }) => {
   );
 }
 
-const mapToProps = ({comments, selectedCommentIndex}) => ({comments, selectedCommentIndex});
+const mapToProps = ({ comments, selectedCommentIndex }) => ({ comments, selectedCommentIndex });
 export default connect(mapToProps)(App);
